3d098cc27131428227630db0e18c2dec24318752
Ben Yelsey <byelsey1@gmail.com>
2016-10-27 09:03:17 -0400

    trying to fix race condition

--- javascripts/wheel.js
+++ javascripts/wheel.js
@@ -74,6 +74,20 @@ $(function() {
     $('#students ul>li').tsort('input', {
         attr: 'value'
     });
+
+    var segments = [];
+    $.each($('#students input:checked'), function(key, cbox) {
+        segments.push(cbox.value);
+    });
+
+    wheel.segments = segments;
+    wheel.init();
+    wheel.update();
+
+    // Hide the address bar (for mobile devices)!
+    setTimeout(function() {
+        window.scrollTo(0, 1);
+    }, 0);
 });
 
 var wheel = {
@@ -229,11 +243,7 @@ var wheel = {
         var centerX = wheel.centerX;
         var centerY = wheel.centerY;
         var size = wheel.size;
-
-        var segments = wheel.segments;
-        var len = wheel.segments.length;
-
-        var value = segments[key];
+        var value = wheel.segments[key];
 
         ctx.save();
         ctx.beginPath();
@@ -268,7 +278,6 @@ var wheel = {
         var angleCurrent = wheel.angleCurrent;
         var lastAngle = angleCurrent;
 
-        var segments = wheel.segments;
         var len = wheel.segments.length;
 
         var centerX = wheel.centerX;
@@ -309,22 +318,4 @@ var wheel = {
     }
 };
 
-window.onload = function() {
-    var segments = [];
-    $.each($('#students input:checked'), function(key, cbox) {
-        segments.push(cbox.value);
-    });
-
-    wheel.segments = segments;
-    wheel.init();
-    wheel.update();
-
-    // Hide the address bar (for mobile devices)!
-    setTimeout(function() {
-        window.scrollTo(0, 1);
-    }, 0);
-};
-
-
 var spectrum = ['#A2395B', '#A63552', '#AA3149', '#AE2D40', '#B22937', '#A23A53', '#924B6F', '#825C8B', '#6F6DA7', '#A63570', '#AC2F5A', '#B22944', '#B8232E', '#C11C17', '#A72A37', '#8D3857', '#734677', '#575597', '#A6358C', '#B43B6A', '#C24148', '#D04726', '#DE5003', '#B84D24', '#924A45', '#6C4766', '#434187', '#A650A0', '#B55A80', '#C46460', '#D36E40', '#E27A1D', '#B26331', '#824C45', '#523559', '#1F1D6D', '#A660AC', '#B67288', '#C68464', '#D69640', '#E6AA19', '#BC892E', '#926843', '#684758', '#3B256D', '#A670B8', '#B8878E', '#CA9E64', '#DCB53A', '#EFCE10', '#C8A628', '#A17E40', '#7A5658', '#502E72', '#80529A', '#98777A', '#B09C5A', '#C8C13A', '#E0E61A', '#C8C13A', '#B09C5A', '#98777A', '#80529A', '#502E72', '#675860', '#7E824E', '#95AC3C', '#ACD62A', '#ABBD4D', '#AAA470', '#A98B93', '#A670B8', '#3B256D', '#4C4D60', '#5D7553', '#6E9D46', '#80C837', '#89AE54', '#929471', '#9B7A8E', '#A660AC', '#1F1D6D', '#2A3F5D', '#35614D', '#40833D', '#4CA82B', '#629248', '#787C65', '#8E6682', '#A650A0', '#434187', '#3B536E', '#336555', '#2B773C', '#228B22', '#43763C', '#646156', '#854C70', '#A6358C', '#575597', '#4A678D', '#3D7983', '#308B79', '#229F6E', '#43856E', '#646B6E', '#85516E', '#A63570', '#6F6DA7', '#5C7EA7', '#498FA7', '#36A0A7', '#20B2AA', '#409497', '#607684', '#805871', '#A2395B', '#7F91C3', '#789AC4', '#71A3C5', '#6AACC6', '#60B6CA', '#7493A6', '#887082', '#9C4D5E', '#B22937', '#71A3C5', '#79A9CD', '#81AFD5', '#89B5DD', '#93BDE7', '#9E95B3', '#A96D7F', '#B4454B', '#C11C17', '#60B6CA', '#67ADC9', '#6EA4C8', '#759BC7', '#7F91C3', '#968193', '#AD7163', '#C46133', '#DE5003', '#20B2AA', '#33A1AA', '#4690AA', '#597FAA', '#6F6DA7', '#8B7085', '#A77363', '#C37641', '#E27A1D', '#229F6E', '#2F8D78', '#3C7B82', '#49698C', '#575597', '#7A6A78', '#9D7F59', '#C0943A', '#E6AA19', '#228B22', '#2A793B', '#326754', '#3A556D', '#434187', '#6E646A', '#99874D', '#C4AA30', '#EFCE10', '#4CA82B', '#41863B', '#36644B', '#2B425B', '#1F1D6D', '#4F4F58', '#808244', '#B0B42F', '#E0E61A', '#80C837', '#6FA044', '#5E7851', '#4D505E', '#3B256D', '#57515C', '#747E4C', '#90AA3B', '#ACD62A'];
-
