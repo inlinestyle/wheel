96e74c08b05c61327b0acb7f87970da1991fc1ab
Ben Yelsey <byelsey1@gmail.com>
2016-11-02 18:04:27 -0400

    try varying speed more

--- javascripts/wheel.js
+++ javascripts/wheel.js
@@ -111,7 +111,7 @@ var wheel = {
     // Start the wheel only if it's not already spinning
     if (wheel.timerHandle == 0) {
       wheel.spinStart = new Date().getTime();
-      wheel.maxSpeed = Math.PI / (16 + Math.random()); // Randomly vary how hard the spin is
+      wheel.maxSpeed = Math.PI / (16 + (Math.random() * 10)); // Randomly vary how hard the spin is
       wheel.frames = 0;
       wheel.timerHandle = setInterval(wheel.onTimerTick, wheel.timerDelay);
     }
